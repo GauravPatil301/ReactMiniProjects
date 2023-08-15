666eba67944b390567d6a4971a801aa4c03cb1cc
gauravpatil301 <301gaurav@gmail.com>
2023-08-15 14:29:31 +0530

    updated code

--- todo-list/src/Main.js
+++ todo-list/src/Main.js
@@ -5,20 +5,29 @@ export default function Main() {
     const [tasklist , settasklist] = useState([])
     function addTask() {
         settasklist([...tasklist,taskname])
+        settaskname("")
     }
-    const tasklistcontent = tasklist.map((task) => {
-        return(<div>
+    const tasklistcontent = tasklist.map((task,index) => {
+        return(<div key={index}>
             <p>{task}</p>
-            <button>delete</button>
+            <i className="fa-solid fa-trash-can" onClick={() => deleteTask(index)}></i>
         </div>)
     })
+
+    function deleteTask(index) {
+        let tempArray = [...tasklist]
+        tempArray.splice(index,1)
+        settasklist(tempArray)
+    }
   return (
     <div>
         <div className="row justify-content-center">
             <div className="col-md-5 shadow-lg p-3 mb-5 bg-white rounded">
                 <h1>React TODO List</h1>
-                <input type="text" placeholder='Enter Task' className='form-control' value={taskname} onChange={(e)=>{settaskname(e.taskname.value)}}/>
+                <input type="text" placeholder='Enter Task' className='form-control' value={taskname} onChange={(e)=>{settaskname(e.target.value)}}/>
                 <button className='btn btn-success' onClick={addTask}>ADD</button>
+                <br/>
+                {tasklistcontent}
             </div>
         </div>
     </div>
